42473b53281fefef8d654e5b6fee6811c7caf043
Divyanshu Rawat <Divyanshu76688@gmail.com>
2023-01-20 19:33:57 +0530

    final-complete

--- file.js
+++ file.js
@@ -5,19 +5,20 @@ function addTask() {
   document.querySelector(".input-text").value = "";
   const list = document.createElement("li");
   list.innerHTML =
-    `<div class="list"><span>${data}</span><div> <ion-icon class="check" id="id-${count}" name="checkbox-outline" onclick=check("${count}") ></ion-icon> <ion-icon class="delete" name="trash-outline"  onclick=deletetask() ></ion-icon></div></div>`;
+    `<div class="list"><span>${data}</span><div> <ion-icon class="check" id="id-${count}" name="checkbox-outline" onclick=check("${count}") ></ion-icon> <ion-icon class="delete"  name="trash-outline"  onclick=deletetask('${count}') ></ion-icon></div></div>`;
   document.querySelector(".task").appendChild(list);
+  list.setAttribute("class",`cal--${count}`);
   count++;
 }
 
-function deletetask() {
-  document.querySelector("li").remove();
+function deletetask(del) {
+  document.querySelector(`.cal--${del}`).remove();
 }
 
 function check(id){
-  const val = document.querySelector("#id-${id}");
+  const val = document.querySelector(`#id-${id}`);
   console.log(val)
-  val.style.backgroundColor ="#AACB73";
+  val.style.backgroundColor ="#54B435";
   // val.forEach(element => {
   //   element.style.backgroundColor ="#AACB73";
     
